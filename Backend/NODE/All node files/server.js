55797f757d993438fdabcb6447ee58d60eb78fb4
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-11-16 12:06:09 +0530

    server file

---
+++ Backend/NODE/All node files/server.js
@@ -0,0 +1,3 @@
+// creating a server
+
+const http=require('http'); //importing module - here http is a global module and http is an identifier, require('http') is the core module if it was not then we had to install it using npm 
