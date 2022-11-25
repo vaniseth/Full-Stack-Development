7681441dbc0faa536f4380f89cfd3b415d280e22
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-11-25 23:29:11 +0530

    body parsing

---
+++ Backend/EXPRESS/body parsing.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser,urlencoded({extended: false}));
+
+app.get("/product", (req,res,next)=>{
+    res.send("<form method ='POST' action = '/new-product'> <input type='text' name ='text1'> <input type = 'button' name = 'Click'>");
+})
+
+app.post("/new-product", (req,res)=>{
+    console.log(req.body);
+    // console.log(req.body.text1); use any one req.body or req.body.text1
+    res.send("From new product");
+})
+
+app.listen(3000);
